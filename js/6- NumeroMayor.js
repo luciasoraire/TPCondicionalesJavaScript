a2f7ee2668330feb2aa5d43ee0efc9001f3325ed
Lucia Soraire <luciasoraire@gmail.com>
2022-07-04 12:05:25 -0300

    agregué el punto 7

--- js/6- NumeroMayor.js
+++ js/6- NumeroMayor.js
@@ -2,11 +2,11 @@ document.write("<br>");
 let numeroUno = prompt("Ingrese el primer número");
 let numeroDos = prompt ("Ingrese el segundo número");
 if (numeroUno>numeroDos){
-document.write("El primer número ingresado, es decir, el número "+ parseInt(numeroUno)+ "es el mayor".)
+document.write("El primer número ingresado ("+ parseInt(numeroUno)+ ") es el mayor.");
 }
 else if (numeroUno==numeroDos){
-    document.write("Ambos numeros ingresados son iguales");
+    document.write("Ambos números ingresados son iguales");
 }
 else{
-    document.write("El segundo número ingresado, es decir, el número " + parseInt(numeroDos) + "es el mayor");
+    document.write("El segundo número ingresado (" + parseInt(numeroDos) + ") es el mayor.");
 }
